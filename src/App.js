39d0fef060623ac22630330affa37d6fc9f1b6e8
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-05-21 08:27:06 +0900

    111

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./styles.css";
 
 export default function App() {
+  const words = [
+    "spray",
+    "limit",
+    "elite",
+    "exuaaaaa",
+    "ddddddddddd",
+    "present"
+  ];
+
+  const result = words.filter((word) => word.length < 6);
+
+  console.log(words);
+  console.log(result);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>filter</h1>
     </div>
   );
 }
