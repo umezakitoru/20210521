468f6b927f5f4b828c3e1490bb838cfb268052fb
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-05-23 07:14:26 +0900

    222

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>filter</h1>
+      <h1>三項演算子</h1>
+      {true ? <h1>true</h1> : <h1>false</h1>}
     </div>
   );
 }
